2626f3ddf71303d920ee9b7ffd251d804cff0847
Tikhon20043 <tihonfedoseev65@gmail.ru>
2023-11-13 12:17:03 +0300

    add PZ_3

--- PZ_3_2.py
+++ PZ_3_2.py
@@ -1,17 +1,14 @@
-def find_different_number(a, b, c, d):
-    if a == b == c:
-        return 4
-    elif a == b == d:
-        return 3
-    elif a == c == d:
-        return 2
-    else:
-        return 1
-
 a = int(input("Введите первое число: "))
 b = int(input("Введите второе число: "))
 c = int(input("Введите третье число: "))
 d = int(input("Введите четвертое число: "))
 
-result = find_different_number(a, b, c, d)
-print("Порядковый номер числа, отличного от остальных:", result)
+if a == b and b == c:
+    print("Порядковый номер числа отличного от остальных: 4")
+elif a == b and b == d:
+    print("Порядковый номер числа отличного от остальных: 3")
+elif a == c and c == d:
+    print("Порядковый номер числа отличного от остальных: 2")
+else:
+    print("Порядковый номер числа отличного от остальных: 1")
+
